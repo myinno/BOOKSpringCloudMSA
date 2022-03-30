29d94d776bee3bf11a7f3fcf4ac81f1085e20430
myinno <jcscom13@gmailo.com>
2022-03-31 00:24:29 +0900

    swagger

--- ch05-swagger-services/api/src/main/java/se/magnus/api/composite/product/ProductCompositeService.java
+++ ch05-swagger-services/api/src/main/java/se/magnus/api/composite/product/ProductCompositeService.java
@@ -3,6 +3,12 @@ package se.magnus.api.composite.product;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@Api(description = "REST API for composite product information.")
 public interface ProductCompositeService {
 
     /**
@@ -11,7 +17,17 @@ public interface ProductCompositeService {
      * @param productId
      * @return the composite product info, if found, else null
      */
-    @GetMapping(
+	//mod05 start
+    @ApiOperation(
+            value = "${api.product-composite.get-composite-product.description}",
+            notes = "${api.product-composite.get-composite-product.notes}")
+        @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Bad Request, 소스에 직접 기술한 내용"),
+            @ApiResponse(code = 404, message = "Not found, the specified id does not exist."),
+            @ApiResponse(code = 422, message = "Unprocessable entity, input parameters caused the processing to fails. See response message for more information.")
+        })
+	//mod05 end
+	@GetMapping(
         value    = "/product-composite/{productId}",
         produces = "application/json")
     ProductAggregate getProduct(@PathVariable int productId);
