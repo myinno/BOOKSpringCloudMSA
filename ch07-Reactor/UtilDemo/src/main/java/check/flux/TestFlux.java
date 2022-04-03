c9a54b25cb8cdbadb753a4afef1275abf648c567
myinno <jcscom13@gmailo.com>
2022-04-03 23:58:05 +0900

    :sparkles: 초기등록

---
+++ ch07-Reactor/UtilDemo/src/main/java/check/flux/TestFlux.java
@@ -0,0 +1,29 @@
+package check.flux;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import reactor.core.publisher.Flux;
+
+public class TestFlux {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestFlux main  = new TestFlux();
+		main.testFlux();
+	}
+
+	
+	public void testFlux() {
+		List<Integer> list = new ArrayList<>();
+		
+		Flux.just(1,2,3,4)
+			.filter(n ->n % 2 == 0)
+			.map(n -> n * 2)
+			.log()
+			.subscribe(n -> list.add(n));
+
+		
+		System.out.println(list.toString() + ", size:" +   list.size());
+	}
+}
---
+++ ch07-Reactor/microservices/review-service/src/main/java/se/magnus/microservices/core/review/services/ReviewServiceImpl.java
@@ -0,0 +1,107 @@
+package se.magnus.microservices.core.review.services;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Scheduler;
+import se.magnus.api.core.review.Review;
+import se.magnus.api.core.review.ReviewService;
+import se.magnus.microservices.core.review.persistence.ReviewEntity;
+import se.magnus.microservices.core.review.persistence.ReviewRepository;
+import se.magnus.util.exceptions.InvalidInputException;
+import se.magnus.util.http.ServiceUtil;
+
+@RestController
+public class ReviewServiceImpl implements ReviewService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ReviewServiceImpl.class);
+
+    private final ServiceUtil serviceUtil;
+    private final ReviewRepository repository;
+    private final ReviewMapper mapper;
+    private final Scheduler scheduler;
+    
+    @Autowired
+//    public ReviewServiceImpl(ServiceUtil serviceUtil) {
+//        this.serviceUtil = serviceUtil;
+//    }
+    public ReviewServiceImpl(Scheduler scheduler, ReviewRepository repository, ReviewMapper mapper, ServiceUtil serviceUtil) {
+        this.scheduler = scheduler;	
+        this.repository = repository;	
+        this.mapper = mapper;
+        this.serviceUtil = serviceUtil;
+    }
+    
+    @Override
+    public List<Review> getReviews(int productId) {
+
+        if (productId < 1) throw new InvalidInputException("Invalid productId: " + productId);
+
+//        if (productId == 213) {
+//            LOG.debug("No reviews found for productId: {}", productId);
+//            return  new ArrayList<>();
+//        }
+
+//        List<Review> list = new ArrayList<>();
+//        list.add(new Review(productId, 1, "Author 1", "Subject 1", "Content 1", serviceUtil.getServiceAddress()));
+//        list.add(new Review(productId, 2, "Author 2", "Subject 2", "Content 2", serviceUtil.getServiceAddress()));
+//        list.add(new Review(productId, 3, "Author 3", "Subject 3", "Content 3", serviceUtil.getServiceAddress()));
+
+        if (productId < 1) throw new InvalidInputException("Invalid productId: " + productId);
+
+        List<ReviewEntity> entityList = repository.findByProductId(productId);
+        List<Review> list = mapper.entityListToApiList(entityList);
+        list.forEach(e -> e.setServiceAddress(serviceUtil.getServiceAddress()));
+
+        LOG.debug("getReviews: response size: {}", list.size());
+
+        return list;
+    }
+    @Override
+    public Flux<Review> getReviews(int productId) {
+        if (productId < 1) throw new InvalidInputException("Invalid productId: " + productId);
+
+        return asyncFlux(getByProductId(productId)).log();
+    }
+
+    private <T> Flux<T> asyncFlux(Iterable<T> iterable) {
+        return Flux.fromIterable(iterable).publishOn(scheduler);
+    }    
+
+    protected List<Review> getByProductId(int productId) {
+
+        List<ReviewEntity> entityList = repository.findByProductId(productId);
+        List<Review> list = mapper.entityListToApiList(entityList);
+        list.forEach(e -> e.setServiceAddress(serviceUtil.getServiceAddress()));
+
+        LOG.debug("getReviews: response size: {}", list.size());
+
+        return list;
+    }   
+    
+    @Override
+    public Review createReview(Review body) {
+        try {
+            ReviewEntity entity = mapper.apiToEntity(body);
+            ReviewEntity newEntity = repository.save(entity);
+
+            LOG.debug("createReview: created a review entity: {}/{}", body.getProductId(), body.getReviewId());
+            return mapper.entityToApi(newEntity);
+
+        } catch (DataIntegrityViolationException dive) {
+            throw new InvalidInputException("Duplicate key, Product Id: " + body.getProductId() + ", Review Id:" + body.getReviewId());
+        }
+    }
+    
+    @Override
+    public void deleteReviews(int productId) {
+        LOG.debug("deleteReviews: tries to delete reviews for the product with productId: {}", productId);
+        repository.deleteAll(repository.findByProductId(productId));
+    }
+}
