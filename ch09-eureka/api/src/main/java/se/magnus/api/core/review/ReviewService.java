979d13366a98dfab8896596bf80e616c7f812f53
myinno <jcscom13@gmailo.com>
2022-04-26 00:38:15 +0900

    11

--- ch09-eureka/api/src/main/java/se/magnus/api/core/review/ReviewService.java
+++ ch09-eureka/api/src/main/java/se/magnus/api/core/review/ReviewService.java
@@ -23,29 +23,30 @@ public interface ReviewService {
     Flux<Review> getReviews(@RequestParam(value = "productId", required = true) int productId);
   
     //ch07 start	
-    /**
-     * Sample usage:
-     *
-     * curl -X POST $HOST:$PORT/review \
-     *   -H "Content-Type: application/json" --data \
-     *   '{"productId":123,"reviewId":456,"author":"me","subject":"yada, yada, yada","content":"yada, yada, yada"}'
-     *
-     * @param body
-     * @return
-     */
-    @PostMapping(
-        value    = "/review",
-        consumes = "application/json",
-        produces = "application/json")
+//    /**
+//     * Sample usage:
+//     *
+//     * curl -X POST $HOST:$PORT/review \
+//     *   -H "Content-Type: application/json" --data \
+//     *   '{"productId":123,"reviewId":456,"author":"me","subject":"yada, yada, yada","content":"yada, yada, yada"}'
+//     *
+//     * @param body
+//     * @return
+//     */
+//    @PostMapping(
+//        value    = "/review",
+//        consumes = "application/json",
+//        produces = "application/json")
     Review createReview(@RequestBody Review body);
-    /**
-     * Sample usage:
-     *
-     * curl -X DELETE $HOST:$PORT/review?productId=1
-     *
-     * @param productId
-     */
-    @DeleteMapping(value = "/review")
+
+    //    /**
+//     * Sample usage:
+//     *
+//     * curl -X DELETE $HOST:$PORT/review?productId=1
+//     *
+//     * @param productId
+//     */
+//    @DeleteMapping(value = "/review")
     void deleteReviews(@RequestParam(value = "productId", required = true)  int productId);  
     //ch07 END    
 }
